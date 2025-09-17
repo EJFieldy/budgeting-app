dac8509b7b4c3deaa38bd0703a2e69d33bb88a98
Elliot Field <elliotfield2@gmail.com>
2025-09-17 16:23:59 +0100

    Navigation bar structure and hamburger menu implemented

--- src/components/layout/navigation.tsx
+++ src/components/layout/navigation.tsx
@@ -9,9 +9,52 @@ import clsx from "clsx";
 
 import type { NavigationItem } from "@/types";
 
-const navigation = [
+const navigation: NavigationItem[] = [
     { name: "Dashboard", href: "/" },
     { name: "Budgets", href: "/budget" },
     { name: "Direct Debits", href: "/direct-debits" },
     { name: "Settings", href: "/settings" },
 ];
+
+export default function NavBar() {
+    const location = useLocation();
+
+    const isCurrentPage = (href: string) => {
+        return location.pathname === href;
+    };
+
+    return (
+        <Disclosure as="nav" className="bg-white border-b-1 border-slate-200">
+            {({ open }) => (
+                <>
+                    <div className="px-2 max-w-7xl">
+                        <div className="flex h-16 items-center justify-between">
+                            <div className="flex items-center justify-center">
+                                <DisclosureButton
+                                    className={clsx(
+                                        "sm:hidden p-2 text-slate-600 focus:ring-2 focus:ring-indigo-500 rounded-md outline-none",
+                                        open && "bg-indigo-50"
+                                    )}>
+                                    <Bars3Icon
+                                        aria-hidden="true"
+                                        className={clsx(
+                                            "size-6",
+                                            open ? "hidden" : "block"
+                                        )}
+                                    />
+                                    <XMarkIcon
+                                        aria-hidden="true"
+                                        className={clsx(
+                                            "size-6",
+                                            open ? "block" : "hidden"
+                                        )}
+                                    />
+                                </DisclosureButton>
+                            </div>
+                        </div>
+                    </div>
+                </>
+            )}
+        </Disclosure>
+    );
+}
