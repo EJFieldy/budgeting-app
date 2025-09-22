3bfa7474fd8fc060db81e688f44a4030665c4a7b
Elliot Field <elliotfield2@gmail.com>
2025-09-22 17:17:35 +0100

    Card layout test setup created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import "./App.css";
 import NavBar from "@/components/layout/navigation";
+import Card from "@/components/ui/card";
 
 function App() {
+    const cardTest = [
+        { title: "Current Balance", amount: "£1200" },
+        { title: "Total Budget", amount: "£1500" },
+        { title: "Monthly Income", amount: "£2500" },
+        { title: "Monthly Expenditure", amount: "£1250" },
+    ];
     return (
         <>
             <NavBar />
+
+            {/* Testing card layout */}
+            <div className="bg-white border-y-1 border-slate-200 mt-5">
+                <div className="max-w-7xl mx-auto py-5">
+                    <div className="grid grid-cols-4 gap-4">
+                        {cardTest.map((item) => (
+                            <Card key={item.title} title={item.title}>
+                                <p className="text-slate-700 text-md">
+                                    {item.amount}
+                                </p>
+                            </Card>
+                        ))}
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -8,8 +8,8 @@ const Card = ({ title, className, children }: SimpleCardProps) => {
                 "rounded-lg shadow-lg bg-white border-slate-200 border-1",
                 className ? `${className}` : ""
             )}>
-            <div className="flex flex-col items-center justify-start">
-                <h2 className="text-slate-900">{title}</h2>
+            <div className="flex flex-col items-start justify-center">
+                <h2 className="text-slate-900 text-lg">{title}</h2>
                 <div className="mt-2">{children}</div>
             </div>
         </div>
