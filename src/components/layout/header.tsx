3d1ae17b5c42bdcf012e2d436cf0d54e3751ba38
Elliot Field <elliotfield2@gmail.com>
2025-09-29 12:11:43 +0100

    Desktop / Tablet design implemented. Adjusted sizing of header background. Added absolute positioning to banners

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -42,17 +42,17 @@ const Header = () => {
     };
 
     return (
-        <div className="bg-white border-b-1 border-slate-200">
+        <div className="relative bg-white border-b-1 border-slate-200 h-40 sm:h-50">
             <div className="max-w-7xl mx-auto py-5">
                 <div className="flex flex-col items-center justify-center">
-                    <h5 className="text-[10px] text-slate-500">
+                    <h5 className="text-[10px] sm:text-xs text-slate-500">
                         Available Balance
                     </h5>
-                    <h2 className="text-4xl text-slate-900 pt-2 pb-5 tracking-tight">
+                    <h2 className="text-4xl font-semibold text-slate-900 pt-2 pb-5 tracking-tight">
                         £1250.72
                     </h2>
                 </div>
-                <div className="sm:hidden mx-5 translate-y-1/4">
+                <div className="sm:hidden absolute bottom-0 left-5 right-5 translate-y-1/2">
                     <Card>
                         <div className="grid grid-cols-3 gap-x-2 py-3 px-2 items-center">
                             {cardTest
@@ -81,6 +81,30 @@ const Header = () => {
                                 })}
                         </div>
                     </Card>
+                </div>
+                <div className="hidden absolute bottom-0 translate-y-1/2 right-5 left-5 mx-auto sm:grid sm:grid-cols-3 gap-x-2 max-w-4xl justify-center">
+                    {cardTest
+                        .filter((item) => item.type !== "balance")
+                        .map((item) => {
+                            const style = getCardStyles(item.type);
+                            return (
+                                <Card key={item.title} className="p-5">
+                                    <div className="flex flex-col items-start">
+                                        <div className="size-8 text-indigo-700 mb-2">
+                                            {style.icon}
+                                        </div>
+                                        <div>
+                                            <p className="text-slate-500 text-xs">
+                                                {item.title}
+                                            </p>
+                                            <h5 className="text-xl text-slate-900 font-semibold tracking-tight">
+                                                {item.amount}
+                                            </h5>
+                                        </div>
+                                    </div>
+                                </Card>
+                            );
+                        })}
                 </div>
             </div>
         </div>
