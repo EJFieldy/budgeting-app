f2ecc2fe1f5ea401fb3fb681430d071da5995961
Elliot Field <elliotfield2@gmail.com>
2025-09-30 14:41:48 +0100

    Expense get and post request handlers added

--- server/index.js
+++ server/index.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const app = express();
-port = 3000;
+const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.use(express.json());
+
+let expenses = [];
+let nextId = 1;
+
+app.get("/api/expenses", (req, res) => {
+    res.json(expenses);
+});
+
+app.post("/api/expenses", (req, res) => {
+    const newExpense = {
+        id: nextId++,
+        ...req.body,
+    };
+
+    expenses.push(newExpense);
+    res.status(201).json(newExpense);
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
