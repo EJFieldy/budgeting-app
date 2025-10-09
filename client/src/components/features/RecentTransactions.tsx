bc49b164c661d25196ffce02072e202e86f6be16
Elliot Field <elliotfield2@gmail.com>
2025-10-09 15:40:39 +0100

    Removed unused imports. Altered padding of container for larger screens.

--- client/src/components/features/RecentTransactions.tsx
+++ client/src/components/features/RecentTransactions.tsx
@@ -42,8 +42,8 @@ const RecentTransactions = () => {
 
     return (
         <>
-            <div className="max-w-4xl px-5">
-                <div className="flex flex-col items-center justify-center gap-y-1">
+            <div className="max-w-4xl px-5 lg:px-0 mx-auto">
+                <div className="sm:hidden flex flex-col items-center justify-center gap-y-1">
                     <div className="mb-1">
                         <h1 className="text-lg font-semibold text-slate-900 tracking-tight">
                             Recent Transactions
