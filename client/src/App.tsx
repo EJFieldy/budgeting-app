b0c84a583c5595b766c7197060e31296e31a48a7
Elliot Field <elliotfield2@gmail.com>
2025-10-10 15:59:43 +0100

    Removed transactions component to be replaced with banner component later

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,12 @@
 import "./App.css";
 import NavBar from "@/components/layout/navigation";
 import Header from "@/components/layout/header";
-import RecentTransactions from "@/components/features/RecentTransactions";
 
 function App() {
     return (
         <>
             <NavBar />
             <Header />
-            <RecentTransactions />
         </>
     );
 }
