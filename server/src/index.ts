db92cd30c67bbf5af014c623739260dd28877999
Elliot Field <elliotfield2@gmail.com>
2025-11-20 17:12:54 +0000

    transactionRoutes mounted in main index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,25 +1,13 @@
-import { Prisma, PrismaClient } from "@prisma/client";
 import express from "express";
 import cors from "cors";
+import transactionRoutes from "../src/routes/transactions";
 const app = express();
-const prisma = new PrismaClient();
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.json({ message: "Server is running!" });
-});
-
-app.get("/api/test-db", async (req, res) => {
-    try {
-        await prisma.$connect();
-        res.json({ message: "Database connected successfully!" });
-    } catch (error) {
-        res.status(500).json({ error: "Database connection failed" });
-    }
-});
+app.use("/api/transactions", transactionRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
