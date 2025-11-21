9376bb83b8433a87147a7a3ac0ba0c787bed475e
Elliot Field <elliotfield2@gmail.com>
2025-11-21 17:46:07 +0000

    categoryRoutes mounted in main index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import transactionRoutes from "../src/routes/transactions";
+import categoryRoutes from "../src/routes/categories";
 const app = express();
 const port = 3000;
 
@@ -8,6 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/transactions", transactionRoutes);
+app.use("/api/categories", categoryRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
