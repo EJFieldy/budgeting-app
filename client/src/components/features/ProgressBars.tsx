9c112ef8abdb0cae1063721d6b7f2fcbf04535a3
Elliot Field <elliotfield2@gmail.com>
2025-12-05 17:09:38 +0000

    Imported ProgressBars to be displayed dynamically based on API request

--- client/src/components/features/ProgressBars.tsx
+++ client/src/components/features/ProgressBars.tsx
@@ -1,7 +1,71 @@
 import { useState, useEffect } from "react";
+import type { TransactionTotals } from "@/types/index.ts";
+import ProgressBar from "@/components/ui/CategoryProgressBar";
 
-// TODO: research how to create progress bars with tailwind
-// TODO: import required types and necessary requirements for progress bars
-// TODO: create fetch request for budget information per category
-// TODO: create progress bar structure and layout
-// TODO: start mobile first, then adjust for tablet and desktop screen size
+type CategoryData = TransactionTotals["categories"][number];
+
+const ProgressBarList = () => {
+    const [barData, setBarData] = useState<CategoryData[]>([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const fetchBarData = async () => {
+            try {
+                setLoading(true);
+                const response = await fetch(
+                    "http://localhost:3000/api/categories/summary/demo-bars"
+                );
+                if (!response.ok) {
+                    throw new Error(
+                        `Failed to fetch progress bar data: ${response.status}`
+                    );
+                }
+
+                const data = await response.json();
+                setBarData(data.categories);
+            } catch (error) {
+                console.error(`Error fetching data: ${error}`);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchBarData();
+    }, []);
+
+    if (loading) {
+        return (
+            <>
+                {[1, 2, 3, 4, 5].map((i) => (
+                    <div key={i} className="w-full py-2 animate-pulse">
+                        <div className="flex flex-row items-center justify-between p-1">
+                            <span className="h-3 w-20 bg-gray-300 rounded"></span>
+                            <span className="h-3 w-24 bg-gray-300 rounded"></span>
+                            <span className="h-3 w-8 bg-gray-300 rounded"></span>
+                        </div>
+                        <div className="w-full rounded-full h-3 bg-gray-800/20">
+                            <div className="rounded-full w-1/2 bg-gray-300 h-3"></div>
+                        </div>
+                    </div>
+                ))}
+            </>
+        );
+    }
+
+    return (
+        <>
+            <div className="mb-1">
+                <h1 className="text-lg text-center font-semibold text-slate-900">
+                    Budget Progress
+                </h1>
+            </div>
+            {barData.map((data) => {
+                if (data.expense) {
+                    return <ProgressBar key={data.id} category={data} />;
+                }
+            })}
+        </>
+    );
+};
+
+export default ProgressBarList;
