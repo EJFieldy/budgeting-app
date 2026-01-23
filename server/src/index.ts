c441dc8722522de86a7857d83eff965242749fd6
Elliot Field <elliotfield2@gmail.com>
2026-01-23 17:32:32 +0000

    Add environment variable support to server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import cors from "cors";
+import "dotenv/config";
 import transactionRoutes from "../src/routes/transactions";
 import categoryRoutes from "../src/routes/categories";
 const app = express();
-const port = 3000;
+const port = Number(process.env.PORT) || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -11,6 +12,6 @@ app.use(express.json());
 app.use("/api/transactions", transactionRoutes);
 app.use("/api/categories", categoryRoutes);
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
     console.log(`Server is running on port ${port}`);
 });
