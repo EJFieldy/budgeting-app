b5a5bdc767e5cf742012eadc73ea0f32d061fa7e
Elliot Field <elliotfield2@gmail.com>
2026-01-29 16:42:04 +0000

    Add budgets put request endpoint for updating budget amount

--- server/src/routes/categories.ts
+++ server/src/routes/categories.ts
@@ -252,6 +252,42 @@ router.post("/", async (req, res, next) => {
     }
 });
 
+router.put("/budgets/:id", async (req, res, next) => {
+    try {
+        const id = Number(req.params.id);
+
+        if (isNaN(id) || id <= 0) {
+            return res.status(400).json({ error: "Invalid Id parameter" });
+        }
+
+        const budgetData = req.body.monthlyBudget;
+
+        if (!budgetData) {
+            return res
+                .status(400)
+                .json({ error: "Amount for monthly budget required" });
+        }
+
+        const updatedBudget = await prisma.category.update({
+            where: {
+                id,
+            },
+            data: {
+                monthlyBudget: parseFloat(budgetData),
+            },
+        });
+
+        res.status(200).json(updatedBudget);
+    } catch (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            if (error.code === "P2025") {
+                return res.status(404).json({ error: "Category not found" });
+            }
+        }
+        next(error);
+    }
+});
+
 router.put("/:id", async (req, res, next) => {
     try {
         const id = Number(req.params.id);
