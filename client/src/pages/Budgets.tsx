871a6fdf61435f5465db4782920cf31fbfb16d1b
Elliot Field <elliotfield2@gmail.com>
2026-02-06 16:31:52 +0000

    Update padding at base of screen

--- client/src/pages/Budgets.tsx
+++ client/src/pages/Budgets.tsx
@@ -8,7 +8,7 @@ const BudgetDisplay = ({
     onEdit?: () => void;
 }) => {
     return (
-        <div className="max-w-2xl mx-auto pt-5 pb-10 px-5 sm:px-0">
+        <div className="max-w-2xl mx-auto pt-5 pb-10 sm:pb-20 px-5 sm:px-0">
             <div className="flex flex-col items-center justify-center gap-y-5">
                 <ProgressBarList
                     refreshTrigger={refreshTrigger}
